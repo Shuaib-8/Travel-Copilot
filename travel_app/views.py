302429c288f325902417b5ab0df79f84d2c48552
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2025-07-24 09:29:21 +0100

    Setup base url and view for travel copilot spa

--- travel_app/views.py
+++ travel_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from core.llm_service import get_travel_guidance
 
-# Create your views here.
+def travel_guidance_view(request):
+    """
+    View to handle travel guidance requests.
+    """
+    response = None
+    updated_messages = None
+    
+    if request.method == 'POST':
+        user_message = request.POST.get('user_message', '')
+        messages = request.POST.getlist('messages')  # Assuming messages are sent as a list
+        response, updated_messages = get_travel_guidance(user_message, messages)
+    
+    return render(request, 'travel_app/travel_guidance.html', {
+        'response': response,
+        'messages': updated_messages
+    })
