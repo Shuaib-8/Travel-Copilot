e0c61caa1484e65217286cefc804db887d49f8bf
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2025-07-25 08:31:05 +0100

    Configure js setup to build frontend assets locally htmx tailwind alpine

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './travel_app/templates/**/*.html',
+    './static/src/**/*.js',
+    './**/.py'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
