7528d68c44706f2837d1d43ec2e01f810532133a
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2025-07-27 16:40:15 +0100

    Update template dir in settings to actual up to date location

--- travel_copilot/settings.py
+++ travel_copilot/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'travel_copilot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR /'travel_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
